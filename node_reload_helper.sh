abc12a8152a60c2997d50f40551fb5848c28c8d9
Ahmed Hamed <ahamed@li-6a31b6cc-2e6d-11b2-a85c-904fbabf1421.ibm.com>
2022-10-16 12:51:57 -0400

    updated references

--- node_reload_helper.sh
+++ node_reload_helper.sh
@@ -1,6 +1,6 @@
 mkdir -p ~/bin
 cd ~/bin
-curl -sSL https://raw.github.ibm.com/National-Northeast-1/Node-Reloader/main/node_reload?token=AACTOHOCSZGBAQ64LLK3LGLDGHJUW > node_reload
+curl -sSL https://raw.githubusercontent.com/ahmedhamedibm/Node-Reload/main/node_reload > node_reload
 chmod +x node_reload || sudo chmod +x node_reload
 cd ~
 echo 'export PATH=$PATH":$HOME/bin"' >> ~/.bashrc || sudo echo 'export PATH=$PATH":$HOME/bin"' >> ~/.bashrc || echo 'export PATH=$PATH":$HOME/bin"' >> ~/.zshrc || sudo echo 'export PATH=$PATH":$HOME/bin"' >> ~/.zshrc
